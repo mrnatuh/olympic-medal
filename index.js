dc16ac81b7656c2e3dc522b8197de4d9689fb105
Marcio Renan Natulini <mnatulini@uolinc.com>
2021-07-25 09:20:16 -0300

    Update

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const fs = require('fs').promises;
 const puppeteer = require('puppeteer');
 const { pageExtend } = require('puppeteer-jquery');
+const medalhas = require("./medalhas");
 
 async function writeFile(data) {
     try {
-        await fs.writeFile('data.json', JSON.stringify(data));
+        await fs.writeFile('medalhas-manual.json', JSON.stringify(data));
         console.log(';)');
     } catch (error) {
         console.error(`Got an error trying to write to a file: ${error.message}`);
@@ -20,23 +21,64 @@ async function writeFile(data) {
     });
 
     let jqPage = pageExtend(pageOrg);
-    
+
     // get all li text in the page as an array
     const data = await jqPage
         .jQuery('#medal-standing-table tbody tr')
         .map((id, elm) => {
+            const depara = {
+                "JPN":"JAP",
+                "USA":"EUA",
+                "KOR":"CDS",
+                "ECU":"EQU",
+                "IRI":"IRA",
+                "THA":"TAI",
+                "NED":"HOL",
+                "ROU":"ROM",
+                "GER":"ALE",
+                "KAZ":"CAZ",
+                "MGL":"MOG",
+                "UKR":"UCR"
+            };
+
             const pos = jQuery(elm).find('td:eq(0) strong').text();
-            const country = jQuery(elm).find('td:eq(1)').attr('data-text');
-            const acron = jQuery(elm).find('td:eq(1) .playerTag').attr('country');
-            const gold = parseInt(jQuery(elm).find('td:eq(2) a').text().replace(/\n/gi, "")) || 0;
-            const silver = parseInt(jQuery(elm).find('td:eq(3) a').text().replace(/\n/gi, "")) || 0;
+            let acron = jQuery(elm).find('td:eq(1) .playerTag').attr('country');
+            if (depara.hasOwnProperty(acron)) acron = depara[acron];
+
+            const ouro = parseInt(jQuery(elm).find('td:eq(2) a').text().replace(/\n/gi, "")) || 0;
+            const prata = parseInt(jQuery(elm).find('td:eq(3) a').text().replace(/\n/gi, "")) || 0;
             const bronze = parseInt(jQuery(elm).find('td:eq(4) a').text().replace(/\n/gi, "")) || 0;
             const total = parseInt(jQuery(elm).find('td:eq(5) a').text().replace(/\n/gi, "")) || 0;
-            const rank = parseInt(jQuery(elm).find('td:eq(6)').text().replace(/\n/gi, "")) || 0;
-            return { pos, country, acron, gold, silver, bronze, total, rank };
+            
+            return { acron, ouro, prata, bronze, total };
+        
         }).pojo();
 
-    await writeFile(data);
+    
+    // JSON da tabela
+    let updates = [];
+    data.map(item => {
+        if (medalhas.countries[item.acron]) {
+            updates.push(item.acron);
 
-    process.exit(1);
+            medalhas.countries[item.acron].score = {
+                total: item.total,
+                ouro: item.ouro,
+                prata: item.prata,
+                bronze: item.bronze
+            };
+        } else {
+            delete medalhas.countries[item.acron].score;
+        }
+
+        delete medalhas.countries[item.acron].medals;
+    }); 
+    
+    console.log(updates, updates.length);
+
+    await writeFile(medalhas);
+    
+    //console.log(medalhas);
+
+    process.exit();
 })();
